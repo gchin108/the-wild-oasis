2e44c0f84959970891a4e4dee46f7b6b73ed4e33
Giorgio <gchin90@gmail.com>
2023-10-14 23:00:54 -0700

    fix typo in route

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
-import Sidebar from "./SideBar";
+import Sidebar from "./Sidebar";
 import styled from "styled-components";
 
 const StyledAppLayout = styled.div`
